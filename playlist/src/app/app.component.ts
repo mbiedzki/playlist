eaa13318a034d40703a950c683bb7f40676ada5b
Michal Biedzki <mbiedzki@me.com>
2022-04-10 09:45:34 +0200

    dark mode preference saving, preparing for mobile view

--- playlist/src/app/app.component.ts
+++ playlist/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, ElementRef, HostBinding, Renderer2 } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'playlist';
@@ -14,20 +15,55 @@ export class AppComponent {
   @HostBinding('class') className = '';
 
   toggleControl = new FormControl(false);
+  resizeObservable: Observable<Event> = new Observable<Event>();
+  resizeSubscription: Subscription = new Subscription();
+  public innerWidth: number = 0;
+  public switchToMobile: number = 768;
 
-  constructor(private dialog: MatDialog, private overlay: OverlayContainer, private el: ElementRef, private renderer:Renderer2) { }
+  constructor(private dialog: MatDialog, private overlay: OverlayContainer, private elementref: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    //set initial dark mode
+    this.initMode();
+
+    //on toggle change class of app and class of overlays and of body and save user preference
     this.toggleControl.valueChanges.subscribe((darkMode) => {
-      const darkClassName = 'dark-mode';
-      this.className = darkMode ? darkClassName : '';
-      if (darkMode) {
-        this.overlay.getContainerElement().classList.add(darkClassName);
-        this.renderer.setStyle(this.el.nativeElement.ownerDocument.body, 'backgroundColor', 'grey');
-      } else {
-        this.overlay.getContainerElement().classList.remove(darkClassName);
-        this.renderer.setStyle(this.el.nativeElement.ownerDocument.body, 'backgroundColor', 'white');
-      }
+      this.saveTogglePref(darkMode);
+      this.setMode(darkMode);
     });
+
+    //monitor width to switch to mobile view
+    this.innerWidth = window.innerWidth;
+    this.resizeObservable = fromEvent(window, 'resize');
+    this.resizeSubscription = this.resizeObservable.subscribe(evt => {
+      this.innerWidth = (evt.target as any).innerWidth;
+    });
+  }
+
+  initMode() {
+    const darkMode: boolean = JSON.parse(localStorage.getItem('darkMode') || 'false');
+    this.setMode(darkMode);
+    this.toggleControl.setValue(darkMode);
+  }
+
+  setMode(darkMode: boolean) {
+    const darkClassName = 'dark-mode';
+    this.className = darkMode ? darkClassName : '';
+    if (darkMode) {
+      this.overlay.getContainerElement().classList.add(darkClassName);
+      this.renderer.setStyle(this.elementref.nativeElement.ownerDocument.body, 'backgroundColor', 'grey');
+    } else {
+      this.overlay.getContainerElement().classList.remove(darkClassName);
+      this.renderer.setStyle(this.elementref.nativeElement.ownerDocument.body, 'backgroundColor', 'white');
+    }
+  }
+
+  saveTogglePref(mode: boolean) {
+    localStorage.setItem('darkMode', JSON.stringify(mode));
+    console.log('darkmode preference saved', mode);
+  }
+
+  ngOnDestroy() {
+    this.resizeSubscription.unsubscribe();
   }
 }
